26a3276962b6777b22134db93e0c0b0d7aac93c2
Ruslan Sharifullin <newwavecfy@gmail.com>
2023-01-29 16:42:46 +0300

    initialized RTK Query

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export const shazamCoreApi = createApi({
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-core.p.rapidapi.com/',
+    prepareHeaders: (headers) => {
+      headers.set(
+        'X-RapidAPI-Key',
+        '0f56b2a643msh6a182ff67b192d7p195590jsndf2b62c8a18b'
+      )
+
+      return headers
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => 'v1/charts/world' }),
+  }),
+})
+
+export const { useGetTopChartsQuery } = shazamCoreApi
