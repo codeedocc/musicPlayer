40882ae5c04da5fca0a0026b39bb85759e17d992
Ruslan Sharifullin <newwavecfy@gmail.com>
2023-01-30 20:30:28 +0300

    finished displaying info in Top Artists and Top Charts

--- src/components/RelatedSongs.jsx
+++ src/components/RelatedSongs.jsx
@@ -10,7 +10,7 @@ const RelatedSongs = ({
   handlePauseClick,
   handlePlayClick,
 }) => (
-  <div className="flex flex-col">
+  <div className="flex flex-col mt-5">
     <h1 className="font-bold text-3xl text-white">Related Songs:</h1>
 
     <div className="mt-6 w-full flex flex-col">
