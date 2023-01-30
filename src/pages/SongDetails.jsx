ba4bed8efa32563efbfbfe957713f50b6822f9a2
Ruslan Sharifullin <newwavecfy@gmail.com>
2023-01-30 16:17:29 +0300

    created Related Songs component with ability to listen to music

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -4,7 +4,10 @@ import { useSelector, useDispatch } from 'react-redux'
 import { DetailsHeader, Error, Loader, RelatedSongs } from '../components'
 
 import { setActiveSong, playPause } from '../redux/features/playerSlice'
-import { useGetSongDetailsQuery } from '../redux/services/shazamCore'
+import {
+  useGetSongDetailsQuery,
+  useGetSongRelatedQuery,
+} from '../redux/services/shazamCore'
 
 const SongDetails = () => {
   const dispatch = useDispatch()
@@ -13,8 +16,16 @@ const SongDetails = () => {
 
   const { data: songData, isFetching: isFetchingSongDetails } =
     useGetSongDetailsQuery({ songid })
+  const {
+    data,
+    isFetching: isFetchinRelatedSongs,
+    error,
+  } = useGetSongRelatedQuery({ songid })
 
-  console.log(songData)
+  if (isFetchingSongDetails || isFetchinRelatedSongs)
+    return <Loader title="Searching song details" />
+
+  if (error) return <Error />
 
   const handlePauseClick = () => {
     dispatch(playPause(false))
@@ -49,6 +60,14 @@ const SongDetails = () => {
           )}
         </div>
       </div>
+
+      <RelatedSongs
+        data={data}
+        isPlaying={isPlaying}
+        activeSong={activeSong}
+        handlePauseClick={handlePauseClick}
+        handlePlayClick={handlePlayClick}
+      />
     </div>
   )
 }
